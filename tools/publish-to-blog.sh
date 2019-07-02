64f3f5058bcac83a91b3af48fdd7fef43cc04c3c
giacomelli <giacomelli@gmail.com>
2019-07-02 19:43:32 -0300

    #unitytips Museum updated

--- tools/publish-to-blog.sh
+++ tools/publish-to-blog.sh
@@ -1,10 +1,6 @@
-cd ../src/UnityTipsMuseum/
-dotnet publish -c release
-cp  -R bin/Release/netstandard2.0/publish/UnityTipsMuseum/dist/  ../../../giacomelli.github.io-jekyll/src/apps/unitytips-museum
-rm -rf bin/
-
-cd ../../tools
-cp -R jekyll-files/ ../../giacomelli.github.io-jekyll/src/apps/unitytips-museum
-
 ./test-on-jekyll.sh
 
+git status
+git add .
+git commit -m"#unitytips Museum updated"
+git push
